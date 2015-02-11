74cbc5b9e83a33561ebbdaedf281c707b83981fd
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-11 14:43:16 -0700

    init commander CLI

--- bin/irjs_apps.js
+++ bin/irjs_apps.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 
 var apps = require('../lib'),
-    program = require('commander')
+	program = require('commander'),
+	host_name = 'localhost',
+	port_number = 27017,
+	db_name = 'play_apps',
+	collection_name = 'listing'
 
 program
-    .version('0.0.1')
-    .usage('<keywords>')
-    .parse(process.argv);
+	.version('0.0.1')
+	.description('Scrape the Google Play Store listing details webpage' +
+		' and store the details in MongoDB documents.')
+    .option('-f, --find', 'package_name')
+	.option('-i, --insert', 'package_name')
+	.option('-u, --update', 'package_name field_to_update value_to_update')
+    .option('-d, --delete', 'package_name')
+	.parse(process.argv);
 
-if(!program.args.length) {
-    program.help();
+if (!program.args.length) {
+	program.help();
 } else {
-	console.log('Test')
-    console.log('Keywords: ' + program.args);   
+	if (program.insert) console.log(' Insert');
+	if (program.update) console.log(' Update');
+	if (program.insert) console.log(' Delete');
 }
