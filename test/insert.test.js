86818d39394cda085324be3e2bbdfb5299b9831d
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-11 12:50:11 -0700

    fix insert

--- test/insert.test.js
+++ test/insert.test.js
@@ -7,16 +7,12 @@ var host_name = 'localhost',
 	collection_name = 'listing'
 
 describe('insert', function() {
-
-	it('should insert a document into mongodb', function() {
-
-		apps.scrape('com.evernote', function(app_info) {
-			app_info['title'].should.be.equal('Evernote')
-
-			apps.insert(host_name, port_number,
-				db_name, collection_name, app_info, function(doc_id) {
-					should.exist(doc_id)
-				})
-		})
+	it('should insert a document into mongodb', function(done) {
+		apps.insert(host_name, port_number,
+			db_name, collection_name, 'com.google.android.apps.maps', function(error, doc_id) {
+				console.log(doc_id)
+				should.exist(doc_id)
+				done()
+			});
 	})
 })
