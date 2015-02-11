df283b550e75d048c46f198c6c8285f59af65762
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-11 12:55:53 -0700

    fix insert example

--- examples/insert.example.js
+++ examples/insert.example.js
@@ -5,10 +5,11 @@ var host_name = 'localhost',
 	db_name = 'play_apps',
 	collection_name = 'listing'
 
-apps.scrape('com.evernote', function(app_info) {
-
-	apps.insert(host_name, port_number,
-		db_name, collection_name, app_info, function(doc_id) {
+apps.insert(host_name, port_number,
+	db_name, collection_name, 'com.facebook.orca', function(error, doc_id) {
+		if (doc_id) {
 			console.log('Inserted a document into the listing collection, _id: ' + doc_id + '.')
-		})
-})
+		} else {
+			console.error('Error: Failed to insert a document into the listing collection.')
+		}
+	})
