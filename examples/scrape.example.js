e2b312034c09e7c5e35477013072aa6bb913a472
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-11 01:36:31 -0700

    refactor

--- examples/scrape.example.js
+++ examples/scrape.example.js
@@ -1,5 +1,5 @@
 var apps = require('../lib')
 
-var title = apps.scrape('com.evernote', function(app_info){
+apps.scrape('com.evernote', function(app_info){
 	console.log(JSON.stringify(app_info))	
 })
