8b578f346f2588360e23bca2032d5e98f81404a1
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-18 09:37:09 -0700

    refactor test

--- test/insert.test.js
+++ test/insert.test.js
@@ -4,15 +4,15 @@ var apps = require('../lib')
 var host_name = 'localhost',
 	port_number = 27017,
 	db_name = 'play_apps',
-	collection_name = 'listing'
+	collection_name = 'listing',
+	package_to_insert = 'com.google.android.apps.maps';
 
 describe('insert', function() {
 	it('should insert a document into mongodb', function(done) {
 		apps.insert(host_name, port_number,
-			db_name, collection_name, 'com.google.android.apps.maps', function(error, doc_id) {
-				console.log(doc_id)
-				should.exist(doc_id)
-				done()
+			db_name, collection_name, package_to_insert, function(error, doc_id) {
+				should.exist(doc_id);
+				done();
 			});
 	})
 })
--- test/scrape.test.js
+++ test/scrape.test.js
@@ -2,7 +2,7 @@ var should = require('should')
 var apps = require('../lib')
 
 
-describe('title', function() {
+describe('scrape', function() {
 
 	it('should return the app title from the Google Play store', function() {
 		apps.scrape('com.evernote', function(app_info) {
